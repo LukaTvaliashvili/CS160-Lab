89063c743001e1c06509df22529629fb9f69f2b8
Luka <lukatvaliashvili@gmail.com>
2024-02-05 21:19:08 +0400

    add prompt for assignment

---
+++ src/main/java/ge/sdsu/assignments/AssignmentOne.java
@@ -0,0 +1,38 @@
+package ge.sdsu.assignments;
+
+public class AssignmentOne {
+
+    /*
+    Your task is to design and implement a simplified online shopping system that focuses on key OOP concepts such as
+    encapsulation, inheritance, polymorphism, and abstraction.
+
+Requirements:
+
+    Classes:
+        Create a class named Product to represent a generic product in the online shopping system. This class should have
+        attributes such as product ID, name, price, and quantity. Implement subclasses for different types of products
+         (e.g., Electronics, Clothing, Books) with specific attributes and behaviors unique to each category.
+         You can add author or ISBN attribute to book, designer for clothing, and manufacturer for electronics. You are
+         free to add some more attributes in accordance to your preferences.
+
+    Encapsulation:
+        Encapsulate the attributes of the Product class and its subclasses appropriately, using private access modifiers
+        and providing public getter and setter methods where necessary.
+
+    Inheritance:
+        Utilize inheritance to create hierarchical relationships between product subclasses.
+        For example, Electronics and Clothing can inherit common attributes and behaviors from the Product superclass.
+
+    Polymorphism:
+        Implement polymorphic behavior by overriding methods such as getDescription() in each subclass to provide
+        category-specific details.
+
+    Shopping Cart:
+        Create a class named ShoppingCart to represent a user's shopping cart. This class should include methods to add
+        products, calculate the total cost, and display the contents of the cart.
+
+    User Interaction:
+        Add main() method and simulate interactions with the online shopping system. Allow users to add products to their
+         shopping cart, view product details, and total cost.
+     */
+}
